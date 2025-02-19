dbb6bc4148993b2f054bc4b4b533f74b25846331
johncarlo-dizon <johncarlovdizon@gmail.com>
2025-02-19 16:35:54 +0800

    void

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:resume/aaron.dart';
+import 'package:resume/forgotpass.dart';
+import 'package:resume/joel.dart';
+import 'package:resume/johncarlo.dart';
+import 'package:resume/jomel.dart';
+import 'package:resume/joseph.dart';
+
+void main() {
+  runApp(CupertinoApp(
+    theme: CupertinoThemeData(brightness: Brightness.light),
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
