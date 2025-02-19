6ad7619930adb90138a9cec41268e12d1c4903c3
Dizon-Joel <joeldizon111111@gmail.com>
2025-02-19 16:56:26 +0800

    start

---
+++ lib/joel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:resume/main.dart';
+
+class JoelPage extends StatefulWidget {
+  const JoelPage({super.key});
+
+  @override
+  State<JoelPage> createState() => _JoelPageState();
+}
