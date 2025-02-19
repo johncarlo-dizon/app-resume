0994e37476bcf05adc89ed43664df56765df62c3
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-02-19 16:52:46 +0800

    my navbar

--- lib/joseph.dart
+++ lib/joseph.dart
@@ -8,3 +8,54 @@ class JosephPage extends StatefulWidget {
   @override
   State<JosephPage> createState() => _JosephPageState();
 }
+
+
+
+class _JosephPageState extends State<JosephPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+
+        navigationBar: CupertinoNavigationBar(
+            middle: Text("Profile", style: TextStyle(fontWeight: FontWeight.bold, color: CupertinoColors.systemBlue),),
+
+
+            leading: CupertinoButton(
+                padding: EdgeInsets.zero,
+                child: Icon(CupertinoIcons.arrow_left_circle, size: 25,), onPressed: (){
+              showCupertinoDialog(context: context, builder: (context){
+                return CupertinoAlertDialog(
+                  content: Column(
+                    children: [
+                      Text("Are you sure you want to log out?"),
+                    ],
+                  ),
+                  actions: [
+
+                    CupertinoDialogAction(
+                        onPressed: (){
+                          Navigator.pop(context);
+                        },
+                        child: Text("Close", style: TextStyle(color: CupertinoColors.destructiveRed),)),
+
+                    CupertinoDialogAction(
+                        onPressed: (){
+                          Navigator.pushAndRemoveUntil(context, CupertinoPageRoute(
+                              builder: (context) => MyApp()), (Route <dynamic> route) => false,);
+                        },
+                        child: Text("Logout", style: TextStyle(color: Colors.blue),)),
+
+
+
+                  ],
+                );
+              });
+            })
+
+
+
+        ),
+       
+       
+  }
+}
