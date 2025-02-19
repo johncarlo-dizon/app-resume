b9eb40d26364b34496936c799923737e29aaefe9
johncarlo-dizon <johncarlovdizon@gmail.com>
2025-02-19 16:39:48 +0800

    navbar resume

---
+++ lib/johncarlo.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:resume/main.dart';
+
+
+class JcPage extends StatefulWidget {
+  const JcPage({super.key});
+
+  @override
+  State<JcPage> createState() => _JcPageState();
+}
+
+class _JcPageState extends State<JcPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+
+        navigationBar: CupertinoNavigationBar(
+          middle: Text("Profile", style: TextStyle(fontWeight: FontWeight.bold, color: CupertinoColors.systemBlue),),
+
+
+          leading: CupertinoButton(
+              padding: EdgeInsets.zero,
+              child: Icon(CupertinoIcons.arrow_left_circle, size: 25,), onPressed: (){
+            showCupertinoDialog(context: context, builder: (context){
+              return CupertinoAlertDialog(
+                content: Column(
+                  children: [
+                          Text("Are you sure you want to log out?"),
+                  ],
+                ),
+                actions: [
+
+
+                  CupertinoDialogAction(
+                      onPressed: (){
+                        Navigator.pop(context);
+                      },
+                      child: Text("Close", style: TextStyle(color: CupertinoColors.destructiveRed),)),
+
+                  CupertinoDialogAction(
+                      onPressed: (){
+                        Navigator.pushAndRemoveUntil(context, CupertinoPageRoute(
+                            builder: (context) => MyApp()), (Route <dynamic> route) => false,);
+                      },
+                      child: Text("Logout", style: TextStyle(color: Colors.blue),)),
+
+
+
+                ],
+              );
+            });
+          })
+
+        ),
+
+
+
+        
+  }
+}
