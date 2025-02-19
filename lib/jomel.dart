dd9ac12596d8fd6d31a2f6077b4e808e8a04e76b
Jomel-Guinto-Simbillo <jomelsimbillo123@gmail.com>
2025-02-19 16:59:46 +0800

    start void

---
+++ lib/jomel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:resume/main.dart';
+
+class JomelPage extends StatefulWidget {
+  const JomelPage({super.key});
+
+  @override
+  State<JomelPage> createState() => _JomelPageState();
+}
