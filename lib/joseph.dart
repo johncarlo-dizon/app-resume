01930eb6d3f0e2c85d3bc808dbf8a2e41efee275
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-02-19 16:51:45 +0800

    start point

---
+++ lib/joseph.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:resume/main.dart';
+
+class JosephPage extends StatefulWidget {
+  const JosephPage({super.key});
+
+  @override
+  State<JosephPage> createState() => _JosephPageState();
+}
