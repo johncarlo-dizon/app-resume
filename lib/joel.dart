19db78fab723f33cc785f67e75202bcf1f6db92f
Dizon-Joel <joeldizon111111@gmail.com>
2025-02-19 16:57:22 +0800

    nav and others

--- lib/joel.dart
+++ lib/joel.dart
@@ -8,3 +8,154 @@ class JoelPage extends StatefulWidget {
   @override
   State<JoelPage> createState() => _JoelPageState();
 }
+
+class _JoelPageState extends State<JoelPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+            middle: Text(
+              "Profile",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: CupertinoColors.systemBlue),
+            ),
+            leading: CupertinoButton(
+                padding: EdgeInsets.zero,
+                child: Icon(
+                  CupertinoIcons.arrow_left_circle,
+                  size: 25,
+                ),
+                onPressed: () {
+                  showCupertinoDialog(
+                      context: context,
+                      builder: (context) {
+                        return CupertinoAlertDialog(
+                          content: Column(
+                            children: [
+                              Text("Are you sure you want to log out?"),
+                            ],
+                          ),
+                          actions: [
+                            CupertinoDialogAction(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text(
+                                  "Close",
+                                  style: TextStyle(
+                                      color: CupertinoColors.destructiveRed),
+                                )),
+                            CupertinoDialogAction(
+                                onPressed: () {
+                                  Navigator.pushAndRemoveUntil(
+                                    context,
+                                    CupertinoPageRoute(
+                                        builder: (context) => MyApp()),
+                                    (Route<dynamic> route) => false,
+                                  );
+                                },
+                                child: Text(
+                                  "Logout",
+                                  style: TextStyle(color: Colors.blue),
+                                )),
+                          ],
+                        );
+                      });
+                })),
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.asset(
+                            'images/joel.png',
+                            height: 120,
+                            width: 120,
+                            fit: BoxFit.cover,
+                          )),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Align(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      "Joel Dizon",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: CupertinoColors.systemBlue),
+                                    ),
+                                  ],
+                                ),
+                                Divider(),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  CupertinoIcons.mail,
+                                  size: 18,
+                                  color: Colors.black,
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                  "joeldizon@gmail.com",
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  CupertinoIcons.number,
+                                  size: 18,
+                                  color: Colors.black,
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                  "(+63) 958-324-4256",
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  CupertinoIcons.globe,
+                                  size: 18,
+                                  color: Colors.black,
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                  "www.codebros.com",
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
